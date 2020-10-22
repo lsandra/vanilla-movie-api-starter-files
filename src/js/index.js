33ab7e7b8035bfd10a12b7772dc6650bcd82b7b4
mr. whalen <michael.whalen@sitechhs.com>
2020-10-22 09:40:19 -0400

    notes

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,23 @@
-
+const pkmn = "alakazam";
+/* function getPkmn() {
+  fetch(`https://pokeapi.co/api/v2/pokemon/luxray`)
+    .then((result) => {
+      //console.log(result);
+      return result.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+}
+getPkmn();
+ */
+async function getPkmn() {
+  try {
+    const result = await fetch(`https://pokeapi.co/api/v2/pokemon/${pkmn}`);
+    const data = await result.json();
+    console.log(data);
+  } catch (error) {
+    alert("Please check spelling");
+  }
+}
+getPkmn();
